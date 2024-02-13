23e83851b28418b35495d3689fb345222a7d6ad0
holyaustin <holyaustin@yahoo.com>
2024-02-13 15:12:05 +0100

    initikal commit

---
+++ scripts/key-gen.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cargo partisia-contract cli account create --file Account-A.pk
+cargo partisia-contract cli account create --file Account-B.pk
+cargo partisia-contract cli account create --file Account-C.pk
